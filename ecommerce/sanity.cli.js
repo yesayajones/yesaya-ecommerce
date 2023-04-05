d7eb0a9b57e0f10752e66b3132f3fe521c636f35
yesayajones <yesayajones@gmail.com>
2023-04-05 18:56:01 +0200

    chore: Add components and logic

---
+++ ecommerce/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'kji7nzc7',
+    dataset: 'production'
+  }
+})
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Toaster } from 'react-hot-toast';
 
-function MyApp() {
-	return <div>Hello there. Are you good.</div>;
+import { Layout } from '../components';
+import '../styles/globals.css';
+import { StateContext } from '../context/StateContext';
+
+function MyApp({ Component, pageProps }) {
+	return (
+		<StateContext>
+			<Layout>
+				<Toaster />
+				<Component {...pageProps} />
+			</Layout>
+		</StateContext>
+	);
 }
 
 export default MyApp;
