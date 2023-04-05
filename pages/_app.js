c3d2302cd83a81e921874f1debaf86bba955022a
yesayajones <yesayajones@gmail.com>
2023-04-05 03:42:36 +0200

    chore: update app and index.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
-import '@/styles/globals.css'
+import React from 'react';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp() {
+	return <div>Hello there</div>;
 }
+
+export default MyApp;
