9c29ae792be871b595d87cf19a4e6cb8273861e4
Yesaya Jones <yesayajones@gmail.com>
2024-04-03 18:58:08 -0500

    chore: Update page title and subtitles

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
 	return (
 		<div className='navbar-container'>
 			<p className='logo'>
-				<Link href='/'>Yesaya Headphones</Link>
+				<Link href='/'>Yesaya Sneakers</Link>
 			</p>
 
 			<button
--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,8 @@ const Home = ({ products, bannerData }) => {
 		<div>
 			<HeroBanner heroBanner={bannerData.length && bannerData[0]} />
 			<div className='products-heading'>
-				<h2>Best Selling Products</h2>
-				<p>Speakers of many variations</p>
+				<h2>Best Selling Sneakers</h2>
+				<p>Sneakers of many variations</p>
 			</div>
 			<div className='products-container'>
 				{products?.map((product) => (
